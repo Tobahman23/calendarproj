af53ca33416413baf1d778bdc03f167dd207379d
Tobahman23 <tobias23.ta@gmail.com>
2022-10-13 14:53:36 +0200

    e
    e

--- index.php
+++ index.php
@@ -6,42 +6,38 @@
 <link rel="stylesheet" href="index.css">
 </head>
 <body>
-<input type="date" id="picker"  value="2022-10-01" min="2022-10-01">
-<form method="post">
-        <input type="submit" name="button1"
-                class="button" value="Previous month" />
-          
-        <input type="submit" name="button2"
-                class="button" value="This month" />
 
-                <input type="submit" name="button3"
-                class="button" value="Next month" />
+    <form method="post" action="okok.php">
+    <label for="birthday">Birthday:</label>
+    <input id="birthday" name="birthday" type="text"/>
+    <label for="name">Name:</label>
+    <input id="name" name="name" type="text"/>
+    <input type="submit" name="subm" value="Done"/>
     </form>
+
 <?php
+echo `whoami`;
+error_reporting(E_ALL); 
+ini_set('display_errors','1'); 
 include 'namnsdag.php';
-$thisdate = date('Y-m-d');
-$monthdays = cal_days_in_month(CAL_GREGORIAN, date('m') , date('Y'));
+include 'birthdays.txt';
+$thisd = $_GET['date'] ?? date('Y-m-d');
+
+$thisdate=date($thisd); 
+
+$thisdatum = strtotime($thisdate);
+
+$monthdays = cal_days_in_month(CAL_GREGORIAN, date('m', strtotime($thisdate)) , date('Y', strtotime($thisdate)));
 $image = '<img src="spooky.webp" alt="spooky">';
-if(isset($_POST['button1'])) {
-    $thisdate = date('Y-m-d', strtotime($thisdate. "- 1 months"));   //kan bara ta bort eller lägga till en månad pågrund av att php är serverbaserat
-    $monthdays = cal_days_in_month(CAL_GREGORIAN, date('m', strtotime(" - 1 months")) , date('Y'));
-    $image = '<img src="september.jpeg">';
-}
-else if(isset($_POST['button2'])) {
-    $thisdate = $thisdate;
-    $image = '<img src="spooky.webp">';
-}
-else if(isset($_POST['button3'])) {
-    $thisdate = date('Y-m-d', strtotime($thisdate. "+ 1 months"));
-    $monthdays = cal_days_in_month(CAL_GREGORIAN, date('m', strtotime(" + 1 months")) , date('Y'));
-    $image = '<img src="november.jpeg">';
-}
- 
+
+ $prevmonth = date('Y-m-01', strtotime("-1 month", $thisdatum));
+ $nextmonth = date('Y-m-01', strtotime("+1 month", $thisdatum));
+
 
 ob_start();
 $eldiv = '<table id="rep"> ';
 for ($i = 0; $i < $monthdays; $i++){
-$dag = date('d', strtotime($thisdate. "- 1 days"));
+$dag = date('d', strtotime($thisdate. "+ 6 days"));
 
 $dayof = date('l', strtotime($thisdate. "+".$i."days". "- $dag days"));
 $weeknum = date('W', strtotime($thisdate. "+".$i."days". "- 7 days"));
@@ -78,11 +74,67 @@ else{
     $eldiv .= "<td class='dty'> $dateofnamn</td>";
 }
  $eldiv .= "</tr>";
+if($months == "Oct")
+{
+    $image = '<img src="spooky.webp" alt="october">';
+}
+else if($months == "Nov")
+{
+    $image = '<img src="november.jpeg" alt="november">';
+}
+else if($months == "Dec")
+{
+    $image = '<img src="december.jpeg" alt="december">';
+}
+else if($months == "Jan")
+{
+    $image = '<img src="january.jpeg" alt="january">';
+}
+else if($months == "Feb")
+{
+    $image = '<img src="february.jpeg" alt="february">';
+}
+else if($months == "Mar")
+{
+    $image = '<img src="march.jpeg" alt="march">';
+}
+else if($months == "Apr")
+{
+    $image = '<img src="april.jpeg" alt="april">';
+}
+else if($months == "May")
+{
+    $image = '<img src="may.jpeg" alt="may">';
+}
+else if($months == "Jun")
+{
+    $image = '<img src="june.jpeg" alt="june">';
+}
+else if($months == "Jul")
+{
+    $image = '<img src="july.jpeg" alt="july">';
+}
+else if($months == "Aug")
+{
+    $image = '<img src="august.jpeg" alt="august">';
+}
+else {
+    $image = '<img src="september.jpeg" alt="september">';
+}
 }
 $eldiv .= "</table>";
 preg_replace('/<table>.*?</table>/s', '', $eldiv);
 echo $image;
 echo $eldiv;
+ 
+#$myfile = fopen("birthdays.txt", "a+") or die("unable");
+#$txt = $_POST['birthday']." ".$_POST['name'];
+#fwrite($myfile, $txt);
+#fclose($myfile);
+
 ?>
+   <br><br>
+  <a href="<?php echo '?date='.$prevmonth ?>" class="button">Previous Month</a>
+  <a href="<?php echo '?date='.$nextmonth ?>" class="button">Next <br>Month</a>
 </body>
 </html>
---
+++ okok.php
@@ -0,0 +1,8 @@
+<?php 
+error_reporting(E_ALL); 
+ini_set('display_errors','1'); 
+$nope = fopen("birthdays.txt", "a+") or die("error");
+$text = $_POST['birthday']." ".$_POST['name'];
+fwrite($nope, $text);
+fclose($nope);
+ ?>
